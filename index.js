1d8f6746981304cf524e56a4683b6eb4640e9295
Scott Corgan <scottcorgan@gmail.com>
2013-12-26 14:15:16 -0800

    Add more methods

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var element = require('dom-element');
 
-var children = function () {
-  
+var children = function (el) {
+  return element(el).children;
+};
+
+children.count = function (ch) {
+  return ch.length;
+};
+
+children.first = function (el) {
+  var domEl = element(el);
+  return domEl.firstChild;
 };
 
 children.last = function (el) {
--- test/index.js
+++ test/index.js
@@ -2,6 +2,28 @@ var children = require('../index.js');
 var test = require('tape');
 var domify = require('domify');
 
+test('returns children of string DOM element', function (t) {
+  var c = children('<div><span class="child1"></span><span class="child2"></span></div>');
+  
+  t.ok(c.length === 2);
+  t.end();
+});
+
+test('returns the number of children', function (t) {
+  var c = children('<div><span class="child1"></span><span class="child2"></span></div>');
+  
+  t.ok(children.count(c) === 2);
+  t.end();
+});
+
+test('returns first child of DOM element', function (t) {
+  var domEl = domify('<div><span class="child1"></span><span class="child2"></span></div>');
+  var firstChild = children.first(domEl);
+  
+  t.equal(firstChild.className, 'child1');
+  t.end();
+});
+
 test('returns last child of DOM element', function (t) {
   var domEl = domify('<div class="parent"><div class="child1"></div><div class="child2"></div></div>');
   var lastChild = children.last(domEl);
